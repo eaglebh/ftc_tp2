f48e20251af1a17b216a0adc47099f7b21aa891c
Pablo Marcondes Fonseca <pablo.fonseca@empresa1.com.br>
2013-06-13 18:33:42 -0300

    adicionando scripts de execucao dos testes

---
+++ executarTestes.bat
@@ -0,0 +1,20 @@
+ftc_tp2 ..\caso1.apd _        sim
+ftc_tp2 ..\caso1.apd 0        nao
+ftc_tp2 ..\caso1.apd 01       nao
+ftc_tp2 ..\caso1.apd 011      sim
+ftc_tp2 ..\caso1.apd 1        nao
+ftc_tp2 ..\caso1.apd 10       nao
+
+ftc_tp2 ..\caso2.apd _        nao
+ftc_tp2 ..\caso2.apd 0        sim
+ftc_tp2 ..\caso2.apd 101      sim
+ftc_tp2 ..\caso2.apd 21012    sim
+ftc_tp2 ..\caso2.apd 21021    nao
+ftc_tp2 ..\caso2.apd 11       nao
+
+ftc_tp2 ..\caso3.apd _        nao
+ftc_tp2 ..\caso3.apd 0        nao
+ftc_tp2 ..\caso3.apd 01       nao
+ftc_tp2 ..\caso3.apd 0111     sim
+ftc_tp2 ..\caso3.apd 1        sim
+ftc_tp2 ..\caso3.apd 10       nao
